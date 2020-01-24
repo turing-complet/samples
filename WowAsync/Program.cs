e4b63372c3a7d04dc61af26f032e7ffd18feaefa
jon <jhagg314@gmail.com>
2020-01-24 01:38:07 -0800

    async sample

---
+++ WowAsync/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace WowAsync
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var loop = BackgroundLoop();
+            while (true)
+            {
+                Console.WriteLine("Wow.");
+                await Task.Delay(1000);
+            }
+        }
+
+        static async Task BackgroundLoop()
+        {
+            while (true)
+            {
+                Console.WriteLine("Sneaky.");
+                await Task.Delay(1000);
+            }
+        }
+    }
+}
