18922a61acc3b9e6444065afa98e508ee3d38f79
jon <jhagg314@gmail.com>
2020-01-26 17:15:09 -0800

    organization

--- diff/Program.cs
+++ diff/Program.cs
@@ -14,35 +14,11 @@ namespace diff
             var longestCommon = LCS(first, second);
         }
 
-        static IEnumerable<string> CustomSort(this IEnumerable<string> words) 
-            => words.OrderByDescending(s => s, new CustomCompare()) as IEnumerable<string>;
-
         static string LCS(string versionOne, string versionTwo)
         {
-            var seq1 = Utility.Subsequences(versionOne).CustomSort();
-            var seq2 = Utility.Subsequences(versionTwo).CustomSort();
-
-            string result;
-            while (true)
-            {
-                string a = seq1.First();
-                string b = seq2.First();
-                if (a == b)
-                {
-                    result = a;
-                    break;
-                }
-                if (CustomCompare.Instance.Compare(a, b) > 0)
-                {
-                    seq1 = seq1.Skip(1);
-                }
-                else
-                {
-                    seq2 = seq2.Skip(1);
-                }
-            }
-
-            return result;
+            var seq1 = Utility.Subsequences(versionOne);
+            var seq2 = Utility.Subsequences(versionTwo);
+            return Utility.LongestCommonElement(seq1, seq2);
         }
     }
 
--- diff/Utility.cs
+++ diff/Utility.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 
 namespace diff
 {
@@ -11,7 +12,7 @@ namespace diff
             return result;
         }
 
-         private static void _subsequences(string input, List<string> result)
+        private static void _subsequences(string input, List<string> result)
         {
             if (string.IsNullOrEmpty(input)) return;
             
@@ -26,6 +27,37 @@ namespace diff
             }
             result.AddRange(tmp);
             result.Add(first);
+        }
+
+        public static IEnumerable<string> CustomSort(this IEnumerable<string> words) 
+            => words.OrderByDescending(s => s, new CustomCompare()) as IEnumerable<string>;
+
+        public static string LongestCommonElement(IEnumerable<string> seq1, IEnumerable<string> seq2)
+        {
+            seq1 = seq1.CustomSort();
+            seq2 = seq2.CustomSort();
+
+            string result;
+            while (true)
+            {
+                string a = seq1.First();
+                string b = seq2.First();
+                if (a == b)
+                {
+                    result = a;
+                    break;
+                }
+                if (CustomCompare.Instance.Compare(a, b) > 0)
+                {
+                    seq1 = seq1.Skip(1);
+                }
+                else
+                {
+                    seq2 = seq2.Skip(1);
+                }
+            }
+
+            return result;
         }
     }
 }
