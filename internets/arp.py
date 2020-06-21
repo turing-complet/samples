44495d2ea5ab23dc9f40d0cd1deaf98b90953b23
jon <jhagg314@gmail.com>
2020-06-21 00:34:15 -0700

    game of life

--- internets/arp.py
+++ internets/arp.py
@@ -1,5 +1,5 @@
 from scapy.layers.l2 import Ether, ARP, srp
 import scapy.packet
 
-ans, unans = srp(Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst="192.168.1.0/24"),timeout=2)
-ans.summary(lambda sr: sr[1].sprintf("%Ether.src% %ARP.psrc%") )
+ans, unans = srp(Ether(dst="ff:ff:ff:ff:ff:ff") / ARP(pdst="192.168.1.0/24"), timeout=2)
+ans.summary(lambda sr: sr[1].sprintf("%Ether.src% %ARP.psrc%"))
